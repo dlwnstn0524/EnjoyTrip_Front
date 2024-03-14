94f91d800e1ed3ea73e12b5cd5b098b24d2189af
dlwnstn0524 <ljs052450@gmail.com>
2024-03-14 16:32:31 +0900

    [FEAT] 헤더 컴포넌트화

---
+++ assets/js/component.js
@@ -0,0 +1,11 @@
+window.onload = function includeNav() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    console.log(this);
+      if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("header").innerHTML = this.responseText;
+      }
+  };
+  xhttp.open("GET", "./assets/html/nav.html", true);
+  xhttp.send();
+}
